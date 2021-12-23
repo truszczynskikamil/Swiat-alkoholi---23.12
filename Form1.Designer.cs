07b4a448088eeecc075ce6cb08e2d374b0e28232
truszczynskikamil <93915855+truszczynskikamil@users.noreply.github.com>
2021-12-23 15:19:12 +0100

    Final

---
+++ Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿
+namespace Swiat_Alkoholi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxAdres = new System.Windows.Forms.TextBox();
+            this.textBoxForename = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxAdres
+            // 
+            this.textBoxAdres.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxAdres.ForeColor = System.Drawing.Color.Black;
+            this.textBoxAdres.Location = new System.Drawing.Point(164, 313);
+            this.textBoxAdres.Name = "textBoxAdres";
+            this.textBoxAdres.Size = new System.Drawing.Size(258, 29);
+            this.textBoxAdres.TabIndex = 14;
+            this.textBoxAdres.Text = "Adres";
+            this.textBoxAdres.TextChanged += new System.EventHandler(this.textBoxTelefon_TextChanged);
+            // 
+            // textBoxForename
+            // 
+            this.textBoxForename.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxForename.ForeColor = System.Drawing.Color.Black;
+            this.textBoxForename.Location = new System.Drawing.Point(164, 278);
+            this.textBoxForename.Name = "textBoxForename";
+            this.textBoxForename.Size = new System.Drawing.Size(258, 29);
+            this.textBoxForename.TabIndex = 12;
+            this.textBoxForename.Text = "Nazwisko";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxName.ForeColor = System.Drawing.Color.Black;
+            this.textBoxName.Location = new System.Drawing.Point(164, 243);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(258, 29);
+            this.textBoxName.TabIndex = 11;
+            this.textBoxName.Text = "Imię";
+            this.textBoxName.TextChanged += new System.EventHandler(this.textBoxName_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.DarkCyan;
+            this.label3.Location = new System.Drawing.Point(117, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(712, 532);
+            this.label3.TabIndex = 15;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.AllowDrop = true;
+            this.dateTimePicker1.CalendarForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dateTimePicker1.CalendarMonthBackground = System.Drawing.SystemColors.MenuText;
+            this.dateTimePicker1.CalendarTitleBackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dateTimePicker1.CalendarTitleForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(164, 426);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(159, 29);
+            this.dateTimePicker1.TabIndex = 17;
+            this.dateTimePicker1.Value = new System.DateTime(2021, 12, 20, 0, 0, 0, 0);
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(164, 397);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 26);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Data Urodzenia :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteCustomSource.AddRange(new string[] {
+            "08:09",
+            "09:10",
+            "10:11",
+            "11:12",
+            "12:13",
+            "13:14",
+            "14:15",
+            "15:16",
+            "16:17",
+            "17:18"});
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.comboBox1.Items.AddRange(new object[] {
+            "Wódka",
+            "Whiskey",
+            "Wino czerwone",
+            "Wino białe",
+            "Brandy",
+            "Likier"});
+            this.comboBox1.Location = new System.Drawing.Point(70, 57);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(238, 32);
+            this.comboBox1.TabIndex = 16;
+            this.comboBox1.Text = "Wybór produktów";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Location = new System.Drawing.Point(70, 115);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(238, 212);
+            this.richTextBox1.TabIndex = 17;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(70, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(238, 32);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Dodaj";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(70, 406);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(238, 79);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Wyślij zamówienie";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Aqua;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Location = new System.Drawing.Point(455, 128);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(360, 506);
+            this.panel1.TabIndex = 19;
+            this.panel1.Visible = false;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(926, 664);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBoxAdres);
+            this.Controls.Add(this.textBoxForename);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label3);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximumSize = new System.Drawing.Size(1040, 973);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBoxAdres;
+        private System.Windows.Forms.TextBox textBoxForename;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Swiat_Alkoholi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxTelefon_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+            int data = int.Parse(dateTimePicker1.Value.ToString("yyyyMMdd"));
+            int now = int.Parse(DateTime.Now.ToString("yyyyMMdd"));
+            int wiek = ((now - data) / 10000);
+            if( wiek >= 18)
+            {
+                panel1.Visible = true;
+                button2.Visible = true;
+            }
+            else
+            {
+                panel1.Visible = false;
+                button2.Visible = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text.Length != 0)
+            {
+                richTextBox1.Text += comboBox1.Text;
+                richTextBox1.Text += ", ";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            {
+                string path = @"G:\Zamówienia\" + textBoxName.Text + "_" + textBoxForename.Text + ".txt";
+                string tekst = "Dane klienta: \r\n ";
+                tekst += "Imię: " + textBoxName.Text + " " + "Nazwisko: " + " " + textBoxForename.Text + "\r\n ";
+                tekst += "Adres: " + textBoxAdres.Text + "\r\n";
+                tekst += "Zamówienie: " + richTextBox1.Text + "\r\n";
+                tekst += "Data złożenia zamówienia: " + DateTime.Now.ToString("dddd, dd MM yyyy") + "\r\n";
+                if (textBoxName.Text.Length == 0 || textBoxForename.Text.Length == 0 || textBoxAdres.Text.Length == 0)
+                {
+                    MessageBox.Show("Podaj imię, nazwisko i adres");
+                }
+                else
+                {
+                    if (!File.Exists(path))
+                    {
+                        File.WriteAllText(path, tekst);
+                    }
+                    else
+                    {
+                        string zmiana = "";
+                        zmiana += "Data złożenia zamówienia: " + DateTime.Now.ToString("dddd, dd MM yyyy") + "\r\n";
+                        File.AppendAllText(path, zmiana);
+                    }
+                }
+
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxNumerDomu_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
